fc4c3afdedca5b4a5786cd5c489494b8de604891
Autumn Farris <autumn@autumnfarris.com>
2020-09-02 20:18:14 -0500

    commit

---
+++ src/components/home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.scss';
+
+const home = () => {
+    return (
+       <div id="introduction">
+          <h1>Hi, I'm Autumn.</h1>
+          <p>a web designer</p>
+      </div>
+    );
+}
+
+export default home;
