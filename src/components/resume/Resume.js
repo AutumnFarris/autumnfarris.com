efb922d5656db544502c60103d2caaa7200de6eb
Autumn Farris <autumn@autumnfarris.com>
2020-09-11 10:19:19 -0500

    add add'l experience, photo, and styling changes

--- src/components/resume/Resume.js
+++ src/components/resume/Resume.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import './Resume.scss';
+import Photo from './Photo.jpeg';
 
 class Resume extends Component {
   render() {
     return (
-      <div class="resume">
       <div class='page-wrapper'>
         <div class='row'>
           <div class='main-column'>
@@ -22,7 +22,6 @@ class Resume extends Component {
                   fields.</ul>
                 </div>
               </div>
-                <div id='experience'>
                   <div id='heading-one'>
                     <ul>EXPERIENCE</ul>
                   </div>
@@ -51,8 +50,6 @@ class Resume extends Component {
                         TDD/Unit Testing, Terraform, as well as GitHub, Slack, and Scrum practices.</ul>
                     <ul><b>Participated in daily Scrum events (2018) –– Scrum Master</b></ul>
                   </div>
-                </div>
-                <div id='education'>
                   <div id='heading-one'>
                     <ul>EDUCATION</ul>
                   </div>
@@ -71,7 +68,6 @@ class Resume extends Component {
                   <div id ='heading-two'>
                     <ul><b>Missouri State University</b> –– <i>Communication, B.S.</i></ul>
                   </div>
-                </div>
                   <div id='heading-three'>
                     <ul>2015 - 2017</ul>
                   </div>
@@ -79,11 +75,41 @@ class Resume extends Component {
                     <ul>Major GPA 4.0/4.0</ul>
                     <ul><b>Dean's List of Scholastic Achievement (2015 - 2017) –– Honoree</b></ul>
                   </div>
-              </div>
+                  <div id='heading-one'>
+                    <ul>ADDITIONAL EXPERIENCE</ul>
+                  </div>
+                  <div id='heading-two'>
+                    <ul><b>Bair's Sports Grill</b>, Springfield, MO –– <i>Shift Leader</i></ul>
+                  </div>
+                  <div id='heading-three'>
+                    <ul>June 2013 - May 2018</ul>
+                  </div>
+                  <div id='content'>
+                    <ul>Bartending and product promotion</ul>
+                    <ul>Employee management</ul>
+                    <ul>Created new bar beverage to support World Autism Awareness Day</ul>
+                    <ul><b>Sold $2,500 in gift cards (2016) –– Contest Winner</b></ul>
+                    <ul><b>Employee Of The Month (2015) –– Recipient</b></ul>
+                  </div>
+                  <br />
+                  <div id='heading-two'>
+                    <ul><b>Big Brothers Big Sisters</b>, Springfield, MO –– <i>Intern</i></ul>
+                  </div>
+                  <div id='heading-three'>
+                    <ul>January 2017 - July 2017</ul>
+                  </div>
+                  <div id='content'>
+                    <ul>Donor outreach and new lead acquisition</ul>
+                    <ul>Update company website</ul>
+                    <ul><b>Raised $13,000 for silent auction (2017) –– Surpassed goal by $10,000</b></ul>
+                  </div>
+                </div>
               <div class='second-column'>
                 <div id='contact'>
                   <ul id='phone-number'>+1 (417) 350-2445</ul>
+                  <br />
                   <ul id='email'><a href = "mailto: autumn@autumnfarris.com">autumn@autumnfarris.com</a></ul>
+                  <br />
                   <ul id='linkedin'>linkedin.com/in/​autumnfarris</ul>
                 </div>
                 <div id='Languages'>
@@ -98,7 +124,6 @@ class Resume extends Component {
                     <ul>Elixir</ul>
                     <ul>C#</ul>
                   </div>
-                  <div id='Frameworks/Software'>
                     <div id='heading-one'>
                       <ul>FRAMEWORKS/SOFTWARE</ul>
                     </div>
@@ -113,8 +138,6 @@ class Resume extends Component {
                         <ul>Microsoft Office Suite</ul>
                         <ul>Google Apps</ul>
                       </div>
-                  </div>
-                  <div id='Highlights'>
                     <div id='heading-one'>
                       <ul>HIGHLIGHTS</ul>
                     </div>
@@ -124,13 +147,17 @@ class Resume extends Component {
                         <ul><b>Color Run –– Annual Entrant</b></ul>
                         <ul>5k race each year to support the community</ul>
                         <ul><b>Run for Rangers –– Entrant</b></ul>
-                        <ul>5k race each year to support The United States Military </ul>
+                        <ul>5k race each year to support The United States Military</ul>
+                        <ul><b>Red Ribbon Ride –– Entrant</b></ul>
+                        <ul>12 mile bicycle ride supporting victims of HIV/AIDS</ul>
                       </div>
-                  </div>
+                    <br />
+                    <div class="photo">
+                    </div>
+                    <img src={Photo} alt="Resume Photo" />
                 </div>
               </div>
             </div>
-          </div>
           </div>
     );
   }
