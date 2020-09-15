78d1106db265c350d1ab6f29edc1ae49b2b42731
Autumn Farris <autumn@autumnfarris.com>
2020-09-15 13:46:15 -0500

    travel page

---
+++ src/components/travel/Travel.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import NZ from './photos/NZ.jpg';
+import './Travel.scss';
+
+const Travel = () => {
+    return (
+
+
+        <div align="center">
+          <div>
+          <iframe src="https://www.google.com/maps/d/embed?mid=10JpIjFZ83-HbjU0p1v80XSXIBjdbsWfO" width="740" height="580"></iframe>
+          </div>
+        </div>
+
+    );
+}
+
+export default Travel;
