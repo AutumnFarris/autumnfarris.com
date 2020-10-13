036115aba95f21fff0d7a875ccf3493fd71ae7ab
Autumn Farris <autumn@autumnfarris.com>
2020-10-13 00:33:21 -0500

    styling tweaks and new favicon

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -6,13 +6,13 @@ function Footer () {
   return (
 <div class="container">
       <div class="footer">
-        <ul>
-          <li><SocialMediaIconsReact borderColor="rgba(0,0,0,0.25)" borderWidth="3" borderStyle="solid" icon="mail" iconColor="rgba(255,255,255,1)" backgroundColor="rgba(177,174,150,1)" iconSize="5" roundness="50%" url="mailto: autumn@autumnfarris.com" size="50" /></li>
-          <li><SocialMediaIconsReact borderColor="rgba(0,0,0,0.25)" borderWidth="3" borderStyle="solid" icon="github" iconColor="rgba(255,255,255,1)" backgroundColor="rgba(177,174,150,1)" iconSize="5" roundness="50%" url="https://github.com/autumnfarris" size="50" /></li>
-          <li><SocialMediaIconsReact borderColor="rgba(0,0,0,0.25)" borderWidth="3" borderStyle="solid" icon="linkedin" iconColor="rgba(255,255,255,1)" backgroundColor="rgba(177,174,150,1)" iconSize="5" roundness="50%" url="https://www.linkedin.com/in/autumnfarris/" size="50" /></li>
-          <li><SocialMediaIconsReact borderColor="rgba(0,0,0,0.25)" borderWidth="3" borderStyle="solid" icon="instagram" iconColor="rgba(255,255,255,1)" backgroundColor="rgba(177,174,150,1)" iconSize="5" roundness="50%" url="https://www.instagram.com/autumnlfarris/" size="50" /></li>
-          <li><SocialMediaIconsReact borderColor="rgba(0,0,0,0.25)" borderWidth="3" borderStyle="solid" icon="facebook" iconColor="rgba(255,255,255,1)" backgroundColor="rgba(177,174,150,1)" iconSize="5" roundness="50%" url="https://www.facebook.com/autumn.farris.35" size="50" /></li>
-          <li><SocialMediaIconsReact borderColor="rgba(0,0,0,0.25)" borderWidth="3" borderStyle="solid" icon="pinterest" iconColor="rgba(255,255,255,1)" backgroundColor="rgba(177,174,150,1)" iconSize="5" roundness="50%" url="https://www.pinterest.com/autumn1618/_saved/" size="50" /></li>
+        <ul class="ul-footer">
+          <li class="li-footer"><SocialMediaIconsReact borderColor="rgba(0,0,0,0.25)" borderWidth="3" borderStyle="solid" icon="mail" iconColor="rgba(255,255,255,1)" backgroundColor="rgba(177,174,150,1)" iconSize="5" roundness="50%" url="mailto: autumn@autumnfarris.com" size="50" /></li>
+          <li class="li-footer"><SocialMediaIconsReact borderColor="rgba(0,0,0,0.25)" borderWidth="3" borderStyle="solid" icon="github" iconColor="rgba(255,255,255,1)" backgroundColor="rgba(177,174,150,1)" iconSize="5" roundness="50%" url="https://github.com/autumnfarris" size="50" /></li>
+          <li class="li-footer"><SocialMediaIconsReact borderColor="rgba(0,0,0,0.25)" borderWidth="3" borderStyle="solid" icon="linkedin" iconColor="rgba(255,255,255,1)" backgroundColor="rgba(177,174,150,1)" iconSize="5" roundness="50%" url="https://www.linkedin.com/in/autumnfarris/" size="50" /></li>
+          <li class="li-footer"><SocialMediaIconsReact borderColor="rgba(0,0,0,0.25)" borderWidth="3" borderStyle="solid" icon="instagram" iconColor="rgba(255,255,255,1)" backgroundColor="rgba(177,174,150,1)" iconSize="5" roundness="50%" url="https://www.instagram.com/autumnlfarris/" size="50" /></li>
+          <li class="li-footer"><SocialMediaIconsReact borderColor="rgba(0,0,0,0.25)" borderWidth="3" borderStyle="solid" icon="facebook" iconColor="rgba(255,255,255,1)" backgroundColor="rgba(177,174,150,1)" iconSize="5" roundness="50%" url="https://www.facebook.com/autumn.farris.35" size="50" /></li>
+          <li class="li-footer"><SocialMediaIconsReact borderColor="rgba(0,0,0,0.25)" borderWidth="3" borderStyle="solid" icon="pinterest" iconColor="rgba(255,255,255,1)" backgroundColor="rgba(177,174,150,1)" iconSize="5" roundness="50%" url="https://www.pinterest.com/autumn1618/_saved/" size="50" /></li>
         </ul>
       </div>
 </div>
--- src/components/resume/Resume.js
+++ src/components/resume/Resume.js
@@ -19,11 +19,9 @@ class Resume extends Component {
               <div id='heading-one'>
                 <p>SUMMARY</p>
               </div>
-              <div id='content'>
-                <p>Ambitious clean code developer who thrives on expanding
+                <p>Ambitious clean code developer who thrives on expanding < br/>
                 knowledge and skillsets in both the professional and personal
                 fields.</p>
-              </div>
             </div>
               <div id='heading-one'>
                 <p>EXPERIENCE</p>
@@ -32,95 +30,81 @@ class Resume extends Component {
                 <p><b>Glasswerks</b>, L.A., CA –– <i>Programmer</i></p>
               </div>
               <div id='heading-three'>
-                <ul>November 2018 - May 2019</ul>
+                <p>November 2018 - May 2019</p>
               </div>
-              <div id='content'>
-                <ul>Helped develop and deploy customer order tracking web app.</ul>
-                <ul>Implemented Elixir and TDD skills to test and deploy back end logic.</ul>
-                <ul><b>Created portal used daily (2019) –– Programmer</b></ul>
-              </div>
+                <p>Helped develop and deploy customer order tracking web app.</p>
+                <p>Implemented Elixir and TDD skills to test and deploy back end logic.</p>
+                <p><b>Created portal used daily (2019) –– Programmer</b></p>
               <br />
               <div id='heading-two'>
-                <ul><b>Common Ledger</b>, Wellington, New Zealand –– <i>Intern</i></ul>
+                <p><b>Common Ledger</b>, Wellington, New Zealand –– <i>Intern</i></p>
               </div>
               <div id='heading-three'>
-                <ul>June 2018 - August 2018</ul>
+                <p>June 2018 - August 2018</p>
               </div>
-              <div id='content'>
-                <ul>Assisted in developing and deploying an accounting application.</ul>
-                <ul>Worked closely with developers to manage complex projects.</ul>
-                <ul>Performed in a full stack platform environment using Vue.js, AWS services,
-                    TDD/Unit Testing, Terraform, as well as GitHub, Slack, and Scrum practices.</ul>
-                <ul><b>Participated in daily Scrum events (2018) –– Scrum Master</b></ul>
-              </div>
+                <p>Assisted in developing and deploying an accounting application.</p>
+                <p>Worked closely with developers to manage complex projects.</p>
+                <p>Performed in a full stack platform environment using Vue.js, AWS services,
+                    TDD/Unit Testing, Terraform, as well as GitHub, Slack, and Scrum practices.</p>
+                <p><b>Participated in daily Scrum events (2018) –– Scrum Master</b></p>
               <div id='heading-one'>
-                <ul>EDUCATION</ul>
+                <p>EDUCATION</p>
               </div>
               <div id='heading-two'>
-                <ul><b>Missouri State University</b> –– <i>Computer Information Systems, B.S.</i></ul>
+                <p><b>Missouri State University</b> –– <i>Computer Information Systems, B.S.</i></p>
               </div>
               <div id='heading-three'>
-                <ul>2017 - 2018</ul>
+                <p>2017 - 2018</p>
               </div>
-              <div id='content'>
-                <ul>Major GPA 3.8/4.0</ul>
-                <ul><b>Association of Information Technology Professionals –– Member</b></ul>
-                <ul><b>Dean's List of Scholastic Achievement (2017 - 2018) –– Honoree</b></ul>
-              </div>
+                <p>Major GPA 3.8/4.0</p>
+                <p><b>Association of Information Technology Professionals –– Member</b></p>
+                <p><b>Dean's List of Scholastic Achievement (2017 - 2018) –– Honoree</b></p>
               <br />
               <div id ='heading-two'>
-                <ul><b>Missouri State University</b> –– <i>Communication, B.S.</i></ul>
+                <p><b>Missouri State University</b> –– <i>Communication, B.S.</i></p>
               </div>
               <div id='heading-three'>
-                <ul>2015 - 2017</ul>
+                <p>2015 - 2017</p>
               </div>
-              <div id='content'>
-                <ul>Major GPA 4.0/4.0</ul>
-                <ul><b>Dean's List of Scholastic Achievement (2015 - 2017) –– Honoree</b></ul>
-              </div>
+                <p>Major GPA 4.0/4.0</p>
+                <p><b>Dean's List of Scholastic Achievement (2015 - 2017) –– Honoree</b></p>
               <div id='heading-one'>
-                <ul>ADDITIONAL EXPERIENCE</ul>
+                <p>ADDITIONAL EXPERIENCE</p>
               </div>
               <div id='heading-two'>
-                <ul><b>Bair's Sports Grill</b>, Springfield, MO –– <i>Shift Leader</i></ul>
+                <p><b>Bair's Sports Grill</b>, Springfield, MO –– <i>Shift Leader</i></p>
               </div>
               <div id='heading-three'>
-                <ul>June 2013 - May 2018</ul>
+                <p>June 2013 - May 2018</p>
               </div>
-              <div id='content'>
-                <ul>Bartending and product promotion</ul>
-                <ul>Employee management</ul>
-                <ul>Created new bar beverage to support World Autism Awareness Day</ul>
-                <ul><b>Sold $2,500 in gift cards (2016) –– Contest Winner</b></ul>
-                <ul><b>Employee Of The Month (2015) –– Recipient</b></ul>
-              </div>
+                <p>Bartending and product promotion</p>
+                <p>Employee management</p>
+                <p>Created new bar beverage to support World Autism Awareness Day</p>
+                <p><b>Sold $2,500 in gift cards (2016) –– Contest Winner</b></p>
+                <p><b>Employee Of The Month (2015) –– Recipient</b></p>
               <br />
               <div id='heading-two'>
-                <ul><b>Big Brothers Big Sisters</b>, Springfield, MO –– <i>Intern</i></ul>
+                <p><b>Big Brothers Big Sisters</b>, Springfield, MO –– <i>Intern</i></p>
               </div>
               <div id='heading-three'>
-                <ul>January 2017 - July 2017</ul>
+                <p>January 2017 - July 2017</p>
               </div>
-              <div id='content'>
-                <ul>Donor outreach and new lead acquisition</ul>
-                <ul>Update company website</ul>
-                <ul><b>Raised $13,000 for silent auction (2017) –– Surpassed goal by $10,000</b></ul>
-              </div>
+                <p>Donor outreach and new lead acquisition</p>
+                <p>Update company website</p>
+                <p><b>Raised $13,000 for silent auction (2017) –– Surpassed goal by $10,000</b></p>
             </div>
               <div class='second-column'>
                 <div id='contact'>
-                  <ul id='phone-number'>+1 (417) 350-2445</ul>
+                  <p id='phone-number'>+1 (417) 350-2445</p>
                   <br />
-                  <ul id='email'><a href="mailto: autumn@autumnfarris.com">autumn@autumnfarris.com</a></ul>
+                  <p id='email'><a href="mailto: autumn@autumnfarris.com">autumn@autumnfarris.com</a></p>
                   <br />
-                  <ul id='linkedin'><a href="https://www.linkedin.com/in/autumnfarris/">linkedin.com/in/​autumnfarris</a></ul>
+                  <p id='linkedin'><a href="https://www.linkedin.com/in/autumnfarris/">linkedin.com/in/​autumnfarris</a></p>
                 </div>
-                <div id='Languages'>
                   <div id='heading-one'>
-                    <ul>LANGUAGES</ul>
+                    <p>LANGUAGES</p>
                   </div>
-                  <div id='content'>
-                    <ul>
+                    <ul class="ul-second-col">
                       <li>Html/CSS/SCSS</li>
                       <li>SQL</li>
                       <li>Javascript/JSON/React</li>
@@ -128,38 +112,37 @@ class Resume extends Component {
                       <li>Elixir</li>
                       <li>C#</li>
                     </ul>
-                  </div>
                     <div id='heading-one'>
-                      <ul>FRAMEWORKS/SOFTWARE</ul>
+                      <p>FRAMEWORKS/SOFTWARE</p>
                     </div>
-                      <div id='content'>
-                        <ul>AWS Services</ul>
-                        <ul>Vue.js</ul>
-                        <ul>TDD/Unit Testing</ul>
-                        <ul>Visual Studio/Code</ul>
-                        <ul>GitHub</ul>
-                        <ul>Slack</ul>
-                        <ul>Agile/Scrum</ul>
-                        <ul>Microsoft Office Suite</ul>
-                        <ul>Google Apps</ul>
-                      </div>
+                    <ul class="ul-second-col">
+                      <li>Bootstrap</li>
+                      <li>AWS Services</li>
+                      <li>Vue.js</li>
+                      <li>TDD/Unit Testing</li>
+                      <li>Visual Studio/Code</li>
+                      <li>GitHub</li>
+                      <li>Slack</li>
+                      <li>Agile/Scrum</li>
+                      <li>Microsoft Office Suite</li>
+                      <li>Google Apps</li>
+                    </ul>
                     <div id='heading-one'>
-                      <ul>HIGHLIGHTS</ul>
+                      <p>HIGHLIGHTS</p>
                     </div>
-                      <div id='content'>
-                        <ul><b>MS150 –– Top 40 Fundraiser</b></ul>
-                        <ul>150 mile bicycle ride supporting victims of Multiple Sclerosis</ul>
-                        <ul><b>Color Run –– Annual Entrant</b></ul>
-                        <ul>5k race each year to support the community</ul>
-                        <ul><b>Run for Rangers –– Entrant</b></ul>
-                        <ul>5k race each year to support The United States Military</ul>
-                        <ul><b>Red Ribbon Ride –– Entrant</b></ul>
-                        <ul>12 mile bicycle ride supporting victims of HIV/AIDS</ul>
-                      </div>
+                    <ul class="ul-second-col">
+                      <li><b>MS150 –– Top 40 Fundraiser</b></li>
+                      <li>150 mile bicycle ride supporting victims of Multiple Sclerosis</li>
+                      <li><b>Color Run –– Annual Entrant</b></li>
+                      <li>5k race each year to support the community</li>
+                      <li><b>Run for Rangers –– Entrant</b></li>
+                      <li>5k race each year to support The United States Military</li>
+                      <li><b>Red Ribbon Ride –– Entrant</b></li>
+                      <li>12 mile bicycle ride supporting victims of HIV/AIDS</li>
+                    </ul>
                     <br />
-                    <div id='resume-photo'>
-                      <img src={Photo} alt="Resume" />
-                    </div>
+                <div id='resume-photo'>
+                  <img src={Photo} alt="Resume" />
                 </div>
               </div>
             </div>
